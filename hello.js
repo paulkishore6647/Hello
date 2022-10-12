9900426b9f6d374668f1099ffc5cc269765faa14
Paul kishore <113435741+paulkishore6647@users.noreply.github.com>
2022-10-12 18:40:47 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 const app=require("express")();
 const PORT=process.env.PORT || 3000;
 app.get("",(req,res)=>{
-    res.send("Hello world!");
+    res.send("Hi,paul kishore");
 });
 app.listen(PORT, ()=>{
     console.log('app up at port ${PORT}');
