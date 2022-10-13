b1d715a0afa160753037e5803f953844f6c119b5
Paul kishore <113435741+paulkishore6647@users.noreply.github.com>
2022-10-13 14:14:12 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 const app=require("express")();
 const PORT=process.env.PORT || 3000;
 app.get("",(req,res)=>{
-    res.send("Bhanu loves Dharani");
+    res.send("Ratna loves Ramya");
 });
 app.listen(PORT, ()=>{
     console.log('app up at port ${PORT}');
